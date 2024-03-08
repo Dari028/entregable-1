cca7adea1e757c8969ab2c09064ee66472b66ab3
Dari028 <dario.pantoja@udea.edu.co>
2024-03-07 20:42:51 -0500

    creamos la clase marcapasos cardiacos con sus respectivos metodos y atributos ademas de encapsular todos los atributos de las anteriores clases

--- Grupo3_Dario Pantoja_Josue Garcia .py
+++ Grupo3_Dario Pantoja_Josue Garcia .py
@@ -28,74 +28,111 @@ class Persona:
             
 class ImplantesDentales:
     def __init__(self):
-        self._id = 0
-        self.forma = ""
-        self.sistema_fijacion = ""
-        self.material = ""
-        self.cantidad = 0
+        self.__id = 0
+        self.__forma = ""
+        self.__sistema_fijacion = ""
+        self.__material = ""
+        self.__cantidad = 0
 
     # Metodos para ver y asignar atributos
     def ver_id(self):
-        return self._id
+        return self.__id
 
     def asignar_id(self, _id):
-        self.id = _id
+        self.__id = _id
 
     def ver_forma(self):
-        return self.forma
+        return self.__forma
 
     def asignar_forma(self, forma):
-        self.forma = forma
+        self.__forma = forma
 
     def ver_sistema_de_fijacion(self):
-        return self.sistema_fijacion
+        return self.__sistema_fijacion
 
     def asignar_sistema_de_fijacion(self, sistema_fijacion):
-        self.sistema_de_fijacion = sistema_fijacion
+        self.__sistema_fijacion = sistema_fijacion
 
     def ver_material(self):
-        return self.material
+        return self.__material
 
     def asignar_material(self, material):
-        self.material = material
+        self.__material = material
 
     def ver_cantidad(self):
-        return self.cantidad
+        return self.__cantidad
 
 class protesis_cadera:
-    def __init__(self,):
-        self.id = 0
-        self.material = ""
-        self.sistema_fijacion = ""
-        self.tamaño = 0
-        self.cantidad = 0
+    def __init__(self):
+        self.__id = 0
+        self.__material = ""
+        self.__sistema_fijacion = ""
+        self.__tamaño = 0
+        self.__cantidad = 0
 
     def ver_id(self):
-        return self.id
+        return self.__id
 
     def asignar_id(self, id):
-        self.id = id
+        self.__id = id
 
     def ver_material(self):
-        return self.material
+        return self.__material
 
     def asignar_material(self, material):
-        self.material = material
+        self.__material = material
 
-    def ver_sistema_de_fijacion(self):
-        return self.sistema_fijacion
+    def ver_sistema_fijacion(self):
+        return self.__sistema_fijacion
 
-    def asignar_sistema_de_fijacion(self, sistema_fijacion):
-        self.sistema_de_fijacion = sistema_fijacion
+    def asignar_sistema_fijacion(self, sistema_fijacion):
+        self.sistema_fijacion = sistema_fijacion
 
     def ver_tamaño(self):
-        return self.tamaño
+        return self.__tamaño
 
     def asignar_tamaño(self, tamaño):
-        self.tamaño = tamaño
+        self.__tamaño = tamaño
 
     def ver_cantidad(self):
-        return self.cantidad
+        return self.__cantidad
 
     def asignar_cantidad(self, cantidad):
-        self.cantidad = cantidad
+        self.__cantidad = cantidad
+class MarcapasosCardiacos:
+    def __init__(self):
+        self.__id = 0
+        self.__electrodos = 0
+        self.__tipo_conexion = ""
+        self.__frecuencia_estimulacion = 0
+        self.__cantidad = 0
+
+    def ver_id(self):
+        return self.__id
+
+    def asignar_id(self, id):
+        self.__id = id
+
+    def ver_electrodos(self):
+        return self.__electrodos
+
+    def asignar_electrodos(self, electrodos):
+        self.__electrodos = electrodos
+
+    def ver_tipo_conexion(self):
+        return self.__tipo_conexion
+
+    def asignar_tipo_conexion(self, tipo_conexion):
+        self.__tipo_conexion = tipo_conexion
+
+    def ver_frecuencia_estimulacion(self):
+        return self.__frecuencia_estimulacion
+
+    def asignar_frecuencia_estimulacion(self, frecuencia_estimulacion):
+        self.__frecuencia_estimulacion = frecuencia_estimulacion
+
+    def ver_cantidad(self):
+        return self.__cantidad
+
+    def asignar_cantidad(self, cantidad):
+        self.__cantidad = cantidad
