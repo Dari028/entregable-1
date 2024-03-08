23e5cbf5105c5b6ea90f536007d413f765250568
Dari028 <dario.pantoja@udea.edu.co>
2024-03-07 19:49:05 -0500

    creamos la clase persona

--- Grupo3_Dario Pantoja_Josue Garcia .py
+++ Grupo3_Dario Pantoja_Josue Garcia .py
@@ -1,8 +1,22 @@
-#!/usr/bin/env python
-# coding: utf-8
+class Persona:
+    def __init__(self):
+        self.__nombre = ""
+        self.__cedula = 0
+        self.__sexo = ""
 
-# In[ ]:
+    # Método para ver el nombre
+    def ver_nombre(self):
+        return self.__nombre
 
+    # Método para ver la cedula
+    def ver_cedula(self):
+        return self.__cedula
 
-
+    # Método para asignar la cedula
+    def asignar_cedula(self, cedula):
+        self.__cedula = cedula
+        
+    # Método para asignar el nombre
+    def asignar_nombre(self, nombre):
+        self.__nombre = nombre
 
